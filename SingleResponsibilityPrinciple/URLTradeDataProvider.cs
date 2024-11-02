fe4e9717f3375b13fb8c7a5094aff9f14d38b10d
allielopez03 <alopez2@css.edu>
2024-11-02 11:39:33 -0500

    Request 407 Done

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -9,13 +9,43 @@ namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
+        String url;
+        ILogger logger;
         public URLTradeDataProvider(string url, ILogger logger)
         {
+            this.url = url;
+            this.logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    logger.LogWarning($"Failed to retrive data. Status code: {response.StatusCode}");
+                    throw new Exception($"Error retrieving data from URL:{url}");
+                    // log error and throw an exception if the URL fails
+                }
+                // set up a Stream and StreamReader to access the data
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    String line;
+                    // Read each line of the text file using reader.ReadLine()
+                    // Read until the reader returns a null line
+                    // Add each line to the tradeData list
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+                }
+            }
+            return tradeData;
         }
     }
 }
