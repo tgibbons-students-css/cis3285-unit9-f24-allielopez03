982d2e2e87a8174b9b45e49df4e08de9526ffac7
allielopez03 <alopez2@css.edu>
2024-11-02 15:50:54 -0500

    Request 405 done. I added a method to the console logger in which it allows your to logtoXML

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,19 +1,49 @@
 ﻿using System;
-
+using System.IO;
+using System.Xml.Linq;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
+        private readonly string logFilePath;
+
+        public ConsoleLogger(string logFilePath = "log.xml")
+        {
+            this.logFilePath = logFilePath;
+
+            // Initialize log file if it doesn't exist
+            if (!File.Exists(logFilePath))
+            {
+                new XDocument(new XElement("logs")).Save(logFilePath);
+            }
+        }
+
         public void LogWarning(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("WARN: ", message), args);
+            string formattedMessage = string.Format(message, args);
+            Console.WriteLine("WARN: " + formattedMessage);
+            LogToXml("WARN", formattedMessage);
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine(string.Concat("INFO: ", message), args);
+            string formattedMessage = string.Format(message, args);
+            Console.WriteLine("INFO: " + formattedMessage);
+            LogToXml("INFO", formattedMessage);
+        }
+
+        private void LogToXml(string type, string message)
+        {
+            var logEntry = new XElement("log",
+                new XElement("type", type),
+                new XElement("message", message)
+            );
+
+            XDocument logDoc = XDocument.Load(logFilePath);
+            logDoc.Root?.Add(logEntry);
+            logDoc.Save(logFilePath);
         }
     }
 }
